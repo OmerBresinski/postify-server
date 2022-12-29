30899bdd3ff631a4145b5eb746f37c29a0f3113c
Omer Bresinski <omerbres@Omers-MacBook-Pro.local>
2022-12-29 17:53:55 +0200

    added user protection

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,5 +4,13 @@ import * as Routes from "../routes";
 
 export const api = express.Router();
 
+api.use((req, res, next) => {
+  if (!req.user) {
+    console.log("not logged in");
+    res.status(401).json({ message: "not logged in" });
+  } else {
+    next();
+  }
+});
 api.use("/users", Routes.users);
 api.use("/auth", Routes.auth);
