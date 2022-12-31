dcae2c793a5283bbb35cd859f331f7852b47577c
Omer Bresinski <omerbres@Omers-MacBook-Pro.local>
2022-12-31 20:31:38 +0200

    ai completion

--- src/routes/tweets/tweets.ts
+++ src/routes/tweets/tweets.ts
@@ -2,6 +2,7 @@ import express from "express";
 import type { Request, Response } from "express";
 import { TwitterApi } from "twitter-api-v2";
 import { prismaClient } from "@/utils/prisma/prismaClient";
+import { getCompletionClient } from "@/utils/openai/completionClient";
 
 export const tweets = express.Router();
 
@@ -30,4 +31,23 @@ tweets.post("/", async (req: Request, res: Response) => {
   });
 
   res.status(200).json(scheduledTweet);
+});
+
+tweets.post("/completion", async (req: Request, res: Response) => {
+  const { text } = req.body;
+  const { twitterAccessToken, twitterId } = req.user!;
+  const { v2: twitter } = new TwitterApi(twitterAccessToken);
+
+  const pastTweetsResponse = await twitter.get(`users/${twitterId}/tweets`);
+  const pastTweets = pastTweetsResponse.data.map(
+    (tweet: { text: string }) => tweet
+  );
+
+  const completion = await getCompletionClient().createCompletion({
+    text,
+    pastTweets,
+  });
+  console.log(JSON.stringify(completion.data.choices, null, 2));
+
+  res.status(200).json({ completion: completion.data.choices[0].text });
 });
---
+++ src/utils/openai/completionClient.ts
@@ -0,0 +1,44 @@
+import Config from "@/config";
+import { Configuration, OpenAIApi } from "openai";
+import { prismaClient } from "@/utils/prisma/prismaClient";
+
+interface Tweet {
+  text: string;
+}
+
+interface Instructions {
+  text: string;
+  pastTweets: Tweet[];
+}
+
+const configuration = new Configuration({
+  apiKey: Config.OPEN_AI_KEY,
+  organization: Config.OPEN_AI_ORG,
+});
+
+const getPrompt = ({ text, pastTweets }: Instructions) => {
+  try {
+    const pastTweetsText = pastTweets.map((tweet) => tweet.text).join('"\n"');
+    return `Considering the following past tweets of this twitter user, as well as their sentiment: "${pastTweetsText}",\nwrite a tweet using the same writing style, while ignoring any tags, but which would convey the following text: ${text}\n without tagging anyone or using any hashtags and no quotes`;
+  } catch (ex) {
+    console.log(ex);
+    return "";
+  }
+};
+
+export const getCompletionClient = () => {
+  const openai = new OpenAIApi(configuration);
+
+  return {
+    createCompletion: async ({ text, pastTweets }: Instructions) => {
+      const prompt = getPrompt({ text, pastTweets });
+
+      return openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        max_tokens: 255,
+        n: 4,
+      });
+    },
+  };
+};
